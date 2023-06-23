ef26e95ade53cc6fc8b3005fba0a26d31f249f2c
Vitaliy-90 <savavitalij1@gmail.com>
2023-06-23 16:45:27 +0300

    32131

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,41 +2,38 @@ import throttle from 'lodash.throttle';
 
 const KEY_FORM = 'feedback-form-state';
 
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  input: document.querySelector('input'),
-  textarea: document.querySelector('textarea'),
-};
-
-// const formData = {};
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+const submitButton = document.querySelector('button');
 
 const formSave = JSON.parse(localStorage.getItem(KEY_FORM));
 if (formSave) {
-  refs.input.value = formSave.input;
-  refs.textarea.value = formSave.textarea;
+  input.value = formSave.input;
+  textarea.value = formSave.textarea;
+  submitButton.disabled = !(input.value && textarea.value);
 }
 
-refs.form.addEventListener(
+form.addEventListener(
   'input',
   throttle(() => {
     localStorage.setItem(
       KEY_FORM,
       JSON.stringify({
-        input: refs.input.value,
-        textarea: refs.textarea.value,
+        input: input.value,
+        textarea: textarea.value,
       })
     );
+    submitButton.disabled = !(input.value && textarea.value);
   }, 500)
 );
-refs.form.addEventListener('submit', event => {
+form.addEventListener('submit', event => {
   event.preventDefault();
   console.log({
-    input: refs.input.value,
-    textarea: refs.textarea.value,
+    input: input.value,
+    textarea: textarea.value,
   });
-  if (refs.input.value === '' || refs.textarea.value === '') {
-    return false;
-  }
-  refs.form.reset();
+  form.reset();
   localStorage.removeItem(KEY_FORM);
+  submitButton.disabled = true;
 });
